96e3e817dedb466f3337c0a8eedfc16d05b160ec
Hiroshi Kikuchi <hiroshi.kikuchi@mixi.co.jp>
2014-04-18 13:13:52 +0900

    smallfix

--- XHR/PracticeXHR/script/xhr-step3.js
+++ XHR/PracticeXHR/script/xhr-step3.js
@@ -1,11 +1,14 @@
 $(function(){
-	var path = "http://localhost:8000/result/";
+	var path = location.origin + "/result/";
 
 	$("#bloodTypeBox").on("change", function(){
 		var bloodType = $(this).val();
+		if(bloodType === "default") return;
 		var url = path + "type-" + bloodType + ".txt";
 		$.ajax(url).done(function(result){
 			$("#resultArea").text(result);
+		}).fail(function(error){
+			console.error(error);
 		});
 	});
 });
